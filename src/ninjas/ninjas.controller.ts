f5a88d009d3f1253356c82c78c787a9649344a53
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-03-23 20:26:51 +0100

    controllers

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Query, Body } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+
+    //GET
+    // @Get()
+    // getNinjas(){
+    //     return ['swords', 'start']
+    // }
+
+    // Querys Get
+    @Get()
+    getNinjasQuery(@Query('type') type : string){
+        return {type}
+    }
+
+    @Get(':id')
+    getOneNinja(@Param('id') id: string) {
+        return {id}
+    }
+    
+    //>POST
+    @Post()
+    createNinja(@Body() createNinjaDto: CreateNinjaDto ) {
+        return {name: createNinjaDto.name}
+    }
+
+
+    // PUT
+    @Put(':id')
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+        return {id, name: updateNinjaDto}
+    }
+
+    // Delete
+    @Delete(':id')
+    deleteNinja() {
+        return {play: 'animal'}
+    }
+
+}
